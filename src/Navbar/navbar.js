e2483870c4aa535d0466f11f869e6cf57622bc5a
Supriya Ganesh <supriya@thinkful.com>
2020-03-10 19:44:39 -0400

    active links

--- src/Navbar/navbar.js
+++ src/Navbar/navbar.js
@@ -3,6 +3,11 @@ import {NavLink} from 'react-router-dom'
 import './navbar.css'
 
 class Navbar extends React.Component{
+    state = {
+        active: false
+    }
+
+
     render() {
         return(
             <div className ="Navbar">
@@ -11,21 +16,30 @@ class Navbar extends React.Component{
                     <li key ="Home">
                         <NavLink
                         className ="Nav__link"
-                        to={'/home'}>
+                        to={'/home'}
+                        activeStyle={{
+                            textDecoration: 'underline'
+                        }}>
                             Home
                         </NavLink>
                     </li>
                     <li key ="auditions">
                         <NavLink
                         className = "Nav__link"
-                        to ={'/auditions'}>
+                        to ={'/auditions'}
+                        activeStyle={{
+                            textDecoration: 'underline'
+                        }}>
                             Auditions
                         </NavLink>
                     </li>
                     <li key = "casting">
                         <NavLink
                             className ="Nav__link"
-                            to ={'/casting'}>
+                            to ={'/casting'}
+                            activeStyle={{
+                                textDecoration: 'underline'
+                            }}>
                              Casting
                         </NavLink>
                     </li>
